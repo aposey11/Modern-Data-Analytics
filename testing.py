96bda5d4f3d2eb25904d317fbd25dfad3513508e
Andrew Posey <aposey1115@att.net>
2026-05-22 00:34:53 +0200

    IDK if this will work

---
+++ testing.py
@@ -0,0 +1,15 @@
+starting_view = pdk.ViewState(latitude= 5, longitude= 50, zoom= 10, max_zoom= 16)
+
+site_locations = pdk.Layer(
+    'ScatterplotLayer',
+    sites[['longitude', 'latitude']],
+    auto_highlight=True,
+    get_position=['longitude', 'latitude'],
+    get_radius=1000,
+    get_fill_color=[100,100,200,0],
+    pickable=True
+)
+
+
+design = pdk.Deck(layers= [site_locations], initial_view_state = starting_view)
+st.pydeck_chart(design)
