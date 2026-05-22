637dc8832daed20f0589029900867a7a2283d406
Andrew Posey <aposey1115@att.net>
2026-05-23 00:01:32 +0200

    Checking update

--- app.py
+++ app.py
@@ -18,11 +18,11 @@ points = pdk.Layer(
     pickable=True,
     auto_highlight=True,
     opacity=.3,
-    get_radius=1500
+    get_radius='average_hour_count*75'
 )
 
 view_point = pdk.ViewState(
-    latitude=51, longitude=4.3, controller=True, zoom=7.1
+    latitude=51, longitude=4.3, controller=True, zoom=7.6
 )
 
 
@@ -30,18 +30,22 @@ chart = pdk.Deck(
     points,
     map_style=None,
     initial_view_state=view_point,
-    tooltip={'text': 'Id: {siteid}\nCluster: {cluster}'},
+    tooltip={'text': 'Id: {siteid}\nCluster: {cluster}\nAverage Hour Count: {average_hour_count}'},
     
 )
+st.set_page_config(layout='wide')
 st.title('Cluster Analysis')
+st.subheader('Commuting patterns and averages through the clusters')
 #st.sidebar.selectbox('Try and select an option?', ['Yes', 'No', 'Maybe'])
 
 col1, col2 = st.columns([12,2], gap='small')
 
 with col1:
     st.pydeck_chart(chart, on_select='rerun', selection_mode='multi-object')
-    st.text('Cluster 0 indicates high traffic counts particularly in the morning\nCluster 1 represents low overall traffic counts\nCluster 2 represents a combination')
+    st.write('**Cluster 0** is representative of overall low traffic counts, irregardless of location')
+    st.write('**Cluster 1** represents the highest traffic counts, it also indicates the highest commuting patterns with 8 am and 4 to 5 pm having by far the highest counts')
+    st.write('**Cluster 2** represents a combination of both low traffic and some commuting behavior. This cluster also has more consistent riders during the midday')
 
 with col2:
-    st.text('Cluster 0\n\nCluster 1\n\nCluster2')
+    st.write('🟦 **Cluster 0**\n\n🟥 **Cluster 1**\n\n🟩 **Cluster 2**')
 #event.selection
