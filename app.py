69b6c4038b65debe8757c1508321d2e3f0eade7c
Andrew Posey <aposey1115@att.net>
2026-05-24 18:18:46 +0200

    Last update before editing

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ points = pdk.Layer(
     pickable=True,
     auto_highlight=True,
     opacity=.3,
-    get_radius=1000,
+    get_radius=900,
 )
 
 view_point = pdk.ViewState(
@@ -42,10 +42,12 @@ col1, col2 = st.columns([12,2], gap='small')
 
 with col1:
     st.pydeck_chart(chart, on_select='rerun', selection_mode='multi-object')
-    st.write('**Cluster 0** is representative of the lowest overall traffic counts. From analyzing the map, cluster 0 spots tend to be in more rural locations or areas where few cyclists bike into the city.')
-    st.write('**Cluster 1** represents the highest traffic counts, it also indicates the highest commuting patterns with 8 am and 4 to 5 pm having by far the highest counts. These sites are located in cities/regions with heavy cycling usage or a defined entry points into a city like a bridge where traffic from the region is forced through a single point.')
-    st.write('**Cluster 2** represents a combination of both low traffic and small commuting behavior. This cluster also has more consistent riders during the midday where the hourly dropoff is limited during the mid to early afternoon. These are also commonly located along roads leading into cities with relatively middle level cycling infrastructure or cycling commuting infrastructure, like Bruges and Ghent.')
+    st.write('**Cluster 0** is the second lowest in terms of cycling counts but the difference between it and cluster 1 is notable. These clusters are located at the entry corridors to large cities like Bruges and Ghent. From this it appears that the number of cyclists commuting into these towns is low, which could be due to other alternative modes of transportation like busses or trains carrying the majority of commuters.')
+    st.write('**Cluster 1** contains the overall lowest cycling counts across the clusters. From the map, it is clear that these counters are located in rural areas where it is unlikely they will be commonly used for commuting.')
+    st.write('**Cluster 2** contains the largest counts across all clusters irregardless of the time. These points are not numerous but they are near major entry points into large cycling cities like Leuven and Kortrijk.')
+    st.write('**Cluster 3** is the second highest for cycling counts and is located near smaller cities like Hasselt and Mechelen. While having a lot less cyclists compared to cluster 2, these counts indicate that there is some cycling infratructure within the city and that cycling is a reasonable alternative to driving for commuting purposes in these regions.')
+    st.write('Overall, there is a clear trend in commuting patterns indicating that the location of these counters are on commuting paths for workers.')
 
 with col2:
-    st.write('🟦 **Cluster 0**\n\n🟥 **Cluster 1**\n\n🟩 **Cluster 2**')
+    st.write('🟦 **Cluster 0**\n\n🟥 **Cluster 1**\n\n🟩 **Cluster 2**\n\n ⬛ **Cluster 3**')
 #event.selection
